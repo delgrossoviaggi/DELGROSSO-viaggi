94401d1659b6d9ba1c899d804264e84aa00e697d
delgrossoviaggi <NICOLAPIO-06@OUTLOOK.IT>
2026-07-27 08:53:24 +0200

    Delete gestionale directory

--- gestionale/js/link-sito.js
+++
@@ -1,7 +0,0 @@
-const URL_SITO = "https://www.delgrossoviaggi.it/";
-
-function apriSito(){
-    window.open(URL_SITO,'_blank');
-}
-
-console.log("Collegamento sito attivo");
--- gestionale/js/mobile-check.js
+++
@@ -1,8 +0,0 @@
-// Ottimizzato per accesso smartphone iPhone e tablet
-
-const mobileSupport = true;
-
-console.log(
-'Gestionale Del Grosso compatibile mobile:',
-mobileSupport
-);
