a076b9a2d707d689911ba019c38935b2b99e9c17
delgrossoviaggi <NICOLAPIO-06@OUTLOOK.IT>
2026-09-03 13:40:30 +0200

    Add files via upload

--- GESTIONALE/assets/brandShell-CIWNUkWr.js
+++ GESTIONALE/assets/brandShell-CIWNUkWr.js
@@ -1,4 +1,4 @@
-/* DELGROSSO V37 compatibility bridge: unified navigation is provided by dg-shell-v37.js. */
+/* DELGROSSO V37 compatibility bridge: unified navigation is provided by dg-shell-v41.js. */
 const noop=()=>{};
 export const n=noop;
 export const r=noop;
