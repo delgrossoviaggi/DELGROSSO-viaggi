f1b78abd2e212b9234acdb69f30670ac83648cf3
delgrossoviaggi <NICOLAPIO-06@OUTLOOK.IT>
2026-09-10 11:48:22 +0200

    Add files via upload

--- GESTIONALE/sw.js
+++ GESTIONALE/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dg-gestionale-v49-locked-ui-20260909';
+const CACHE = 'dg-gestionale-v52-supabase-sync-20260910';
 const SCOPE_URL = new URL('./', self.registration.scope);
 const SCOPE = SCOPE_URL.pathname;
 
