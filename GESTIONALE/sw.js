b280b475177b51fe23e5013b9df7475a28800fe9
delgrossoviaggi <NICOLAPIO-06@OUTLOOK.IT>
2026-09-20 15:06:11 +0200

    Add files via upload

--- GESTIONALE/sw.js
+++ GESTIONALE/sw.js
@@ -1,5 +1,5 @@
-/* DEL GROSSO GESTIONALE — V185 ARCHIVE RECOVERY */
-const CACHE=`dg-gestionale-v185`;
+/* DEL GROSSO GESTIONALE — V187 ARCHIVE RECOVERY */
+const CACHE=`dg-gestionale-v187`;
 const BASE=new URL(`./`,self.registration.scope);
 const PRECACHE=[
  `agenda.html`,
